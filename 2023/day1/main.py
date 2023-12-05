1aee4b9c663a39ca908da06a0d7f9d96f75f686e
Allen Lovett <allen.lovett@kainos.com>
2023-12-05 12:57:41 +0000

    day1 part 2

--- 2023/day1/main.py
+++ 2023/day1/main.py
@@ -3,6 +3,24 @@ f = open("input.txt", "r")
 
 coordinates = f.read().split("\n")
 totals = []
-for entry in coordinates:
-    totals.append(int(str(re.findall('\d', entry)[0])+str(re.findall('\d', entry)[-1])))
-print(sum(totals))
+
+def part1():
+    for entry in coordinates:
+        totals.append(int(str(re.findall('\d', entry)[0])+str(re.findall('\d', entry)[-1])))
+    print(sum(totals)) 
+
+def part2():
+    words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    
+    for entry in coordinates:
+        nums = {}
+        for word in words: # first look for any number words
+                if len(re.findall(word, entry)) > 0:
+                    for num in re.finditer(word, entry):
+                        nums[num.start()] = (str(words.index(word)+1))
+        for digit in re.finditer(r"\d", entry):
+             nums[digit.start()] = entry[digit.start()]
+        totals.append(int(sorted(nums.items())[0][1]+sorted(nums.items())[-1][1]))
+    print(sum(totals))
+
+part2()
