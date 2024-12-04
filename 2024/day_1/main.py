0c91caaef63c33f90d0ac1fbe116aea845503821
AllenLovett <allen.g.lovett@gmail.com>
2024-12-04 08:51:30 +0000

    day 1 complete

--- 2024/day_1/main.py
+++ 2024/day_1/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 f = open(os.path.join(sys.path[0],"input.txt"), "r")
 
@@ -13,10 +14,11 @@ for entry in input:
     list_left.append(int(entry.split("   ")[0]))
     list_right.append(int(entry.split("   ")[1]))
 
-def part1():
     # arrange both lists in numerical order to find the most aligned pairs
     sorted_left = sorted(list_left)
     sorted_right = sorted(list_right)
+    
+def part1():
     
     scores = []
     for idx in range(len(list_left)):
@@ -29,4 +31,16 @@ def part1():
         
     print(sum(scores))
 
+def part2():
+    similarity_scores = []
+
+    # For each entry in the left lise
+    # multiply value by number of matching entries in right list
+    for entry in sorted_left:
+        similarity_scores.append(entry * sorted_right.count(entry))
+    
+    print(sum(similarity_scores))
+
+
 part1()
+part2()
